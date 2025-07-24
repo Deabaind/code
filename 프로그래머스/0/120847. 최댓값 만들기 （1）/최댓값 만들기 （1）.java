fbe19621dcebd3904256f176f33ede2b64506f8c
Deabaind <167089259+Deabaind@users.noreply.github.com>
2025-07-24 21:52:09 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.53 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int length = numbers.length;
+        int answer = numbers[length - 1] * numbers[length - 2];
+        return answer;
+    }
+}
