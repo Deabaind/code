c572e20056d7272d140034c0ef5b0b7129241a62
Deabaind <167089259+Deabaind@users.noreply.github.com>
2025-10-16 22:17:32 +0900

    [level 0] Title: 대문자와 소문자, Time: 8.27 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder();
+        char c;
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            c = my_string.charAt(i);
+            if (Character.isUpperCase(c)) {
+                answer.append(Character.toLowerCase(c));
+            } else {
+                answer.append(Character.toUpperCase(c));
+            }
+        }
+        return answer.toString();
+    }
+}
