196d69125e5db5ea93d8f6b966d4fe9f09046e2f
Deabaind <167089259+Deabaind@users.noreply.github.com>
2025-11-28 22:27:02 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        int b1 = Integer.valueOf(bin1, 2);
+        int b2 = Integer.valueOf(bin2, 2);
+        
+        return Integer.toBinaryString(b1 + b2);
+    }
+}
